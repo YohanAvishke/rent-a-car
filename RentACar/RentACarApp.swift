bc6ec64bab9c2b0628363bfe09363ac0c7d566a0
YohanAvishke <yaediriweera@gmail.com>
2025-04-23 15:14:56 +1000

    Initial Commit

---
+++ RentACar/RentACarApp.swift
@@ -0,0 +1,17 @@
+//
+//  RentACarApp.swift
+//  RentACar
+//
+//  Created by Yohan Ediriweera on 2025-04-23.
+//
+
+import SwiftUI
+
+@main
+struct RentACarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
