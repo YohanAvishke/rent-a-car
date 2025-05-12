d54669af8806682c9511de6b1f73847dcf0b4f90
YohanAvishke <yaediriweera@gmail.com>
2025-05-12 23:00:23 +1000

    Use core data in explore view

--- RentACar/Views/ExploreView.swift
+++ RentACar/Views/ExploreView.swift
@@ -1,51 +1,27 @@
-//
-//  ExploreView.swift
-//  RentACar
-//
-//  Created by Gavin Li on 1/5/2025.
-//
-
 import SwiftUI
+import SwiftData
 
 struct ExploreView: View {
     @State private var searchText: String = ""
     @State private var selectedFilter: String? = nil
-
-    struct Car: Identifiable {
-        let id = UUID()
-        let name: String
-        let location: String
-        let pricePerDay: Int
-        let brand: String
-        let imageName: String
-    }
-
-    let cars: [Car] = [
-        Car(name: "Mitsubishi OUTLANDER 2023", location: "Chippendale • 2.3 km", pricePerDay: 186, brand: "Mitsubishi", imageName: "car1"),
-        Car(name: "Tesla Model 3 2022", location: "Sydney • 1.5 km", pricePerDay: 220, brand: "Tesla", imageName: "car2"),
-        Car(name: "BMW X5 2021", location: "Parramatta • 5 km", pricePerDay: 250, brand: "BMW", imageName: "car3")
-    ]
-
+    @Query var vehicles: [Vehicle]
     let filters = ["All", "Mitsubishi", "Tesla", "BMW", "Price < 200"]
-
-    var filteredCars: [Car] {
-        cars.filter { car in
-            let matchesSearch = searchText.isEmpty || car.name.localizedCaseInsensitiveContains(searchText)
+    
+    var filteredVehicles: [Vehicle] {
+        vehicles.filter { vehicle in
+            let name = "\(vehicle.brandName) \(vehicle.modelName) \(vehicle.createdYear)"
+            let matchesSearch = searchText.isEmpty || name.localizedCaseInsensitiveContains(searchText)
             let matchesFilter: Bool
             if let filter = selectedFilter {
                 switch filter {
-                case "Mitsubishi":
-                    matchesFilter = car.brand == "Mitsubishi"
-                case "Tesla":
-                    matchesFilter = car.brand == "Tesla"
-                case "BMW":
-                    matchesFilter = car.brand == "BMW"
-                case "Price < 200":
-                    matchesFilter = car.pricePerDay < 200
-                case "All":
-                    matchesFilter = true
-                default:
-                    matchesFilter = true
+                    case "Mitsubishi", "Tesla", "BMW":
+                        matchesFilter = vehicle.brandName == filter
+                    case "Price < 200":
+                        matchesFilter = vehicle.pricePerDay < 200
+                    case "All":
+                        matchesFilter = true
+                    default:
+                        matchesFilter = true
                 }
             } else {
                 matchesFilter = true
@@ -53,17 +29,16 @@ struct ExploreView: View {
             return matchesSearch && matchesFilter
         }
     }
-
+    
     var body: some View {
-        NavigationView {
+        NavigationStack {
             VStack(alignment: .leading) {
-
-                // 搜索框
+                // Search
                 TextField("Search cars...", text: $searchText)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .padding(.horizontal)
-
-                // 过滤器按钮
+                
+                // Filters
                 ScrollView(.horizontal, showsIndicators: false) {
                     HStack {
                         ForEach(filters, id: \.self) { filter in
@@ -82,17 +57,17 @@ struct ExploreView: View {
                     .padding(.horizontal)
                 }
                 .padding(.bottom, 8)
-
-                // 汽车列表
+                
+                // Vehicle cards
                 ScrollView {
                     VStack(spacing: 20) {
-                        ForEach(filteredCars) { car in
-                            CarCardView(car: car)
+                        ForEach(filteredVehicles) { vehicle in
+                            VehicleCardView(vehicle: vehicle)
                         }
                     }
                     .padding(.horizontal)
                 }
-
+                
                 Spacer()
             }
             .navigationTitle("Explore")
@@ -100,37 +75,34 @@ struct ExploreView: View {
     }
 }
 
-struct CarCardView: View {
-    let car: ExploreView.Car
-
+struct VehicleCardView: View {
+    let vehicle: Vehicle
+    
     var body: some View {
         VStack(alignment: .leading, spacing: 8) {
-            Image(car.imageName)
-                .resizable()
-                .scaledToFill()
-                .frame(height: 200)
-                .clipped()
-                .cornerRadius(12)
-
-            Text(car.name)
+            if let imageName = vehicle.imageUrls.first {
+                Image(imageName)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(height: 200)
+                    .clipped()
+                    .cornerRadius(12)
+            }
+            
+            Text("\(vehicle.brandName) \(vehicle.modelName) \(vehicle.createdYear)")
                 .font(.headline)
-
-            Text(car.location)
+            
+            Text(vehicle.dealer.address)
                 .font(.subheadline)
                 .foregroundColor(.secondary)
-
-            Text("$\(car.pricePerDay)/day")
+            
+            Text("$\(Int(vehicle.pricePerDay))/day")
                 .font(.title3)
                 .bold()
-
         }
         .background(Color.white)
         .cornerRadius(12)
         .shadow(radius: 4)
     }
-}
-
-#Preview {
-    ExploreView()
 }
 
